85665f29a3286e43d5522fc04eec6dc1b61d67c3
obliviusm <obliviusm@gmail.com>
2017-11-25 18:32:48 +0200

    Add few string repeat

--- lib/string_repeat.ex
+++ lib/string_repeat.ex
@@ -4,3 +4,21 @@ defmodule StringRepeat do
     s <> repeat_str(n-1, s)
   end
 end
+
+defmodule StringRepeat2 do
+  def repeat_str(n, s), do: String.duplicate(s,n)
+end
+
+defmodule StringRepeat3 do
+
+  def repeat_str(n, s) do
+    repeat_str(n - 1, s, s <> s)
+  end
+
+  def repeat_str(1, _s, a), do: a
+
+  def repeat_str(n, s, a) do
+    repeat_str(n - 1, s, s <> a)
+  end
+
+end
--- test/string_repeat_test.exs
+++ test/string_repeat_test.exs
@@ -8,3 +8,14 @@ defmodule StringRepeatTest do
     assert repeat_str(2, "ha ") == "ha ha "
   end
 end
+
+defmodule StringRepeatTest2 do
+  use ExUnit.Case
+  import StringRepeat2, only: [repeat_str: 2]
+
+  test "Basic tests" do
+    assert repeat_str(3, "*") == "***"
+    assert repeat_str(5, "#") == "#####"
+    assert repeat_str(2, "ha ") == "ha ha "
+  end
+end
