933eee2361ae063dd8944c8c4bef37cfdb005883
obliviusm <obliviusm@gmail.com>
2017-11-25 21:15:28 +0200

    Part list from end to start

--- lib/partlist.ex
+++ lib/partlist.ex
@@ -12,3 +12,23 @@ defmodule Partlist do
     end
   end
 end
+
+defmodule Partlist2 do
+  defp glue_list(list, divider) do
+    { left_list, right_list } = Enum.split(list, divider)
+    [Enum.join(left_list, " "), Enum.join(right_list, " ")]
+  end
+
+  def part_list(list) do
+    divider = Enum.count(list) - 1
+    part_list(list, divider, [])
+  end
+
+  def part_list(list, 0, lists_acc) do
+    lists_acc
+  end
+
+  def part_list(list, divider, lists_acc) do
+    part_list list, divider - 1,  [glue_list(list, divider) | lists_acc]
+  end
+end
--- test/partlist_test.exs
+++ test/partlist_test.exs
@@ -17,3 +17,23 @@ defmodule PartlistTest do
   end
 
 end
+
+defmodule PartlistTest2 do
+
+  use ExUnit.Case
+
+  defp testPartList(numtest, arr, ans) do
+    IO.puts("Test #{numtest}")
+    assert Partlist2.part_list(arr) == ans
+  end
+
+  test "part_list" do
+    testPartList 1, ["I", "wish", "I", "hadn't", "come"],
+        [["I", "wish I hadn't come"], ["I wish", "I hadn't come"], ["I wish I", "hadn't come"], ["I wish I hadn't", "come"]]
+    testPartList 2, ["cdIw", "tzIy", "xDu", "rThG"],
+        [["cdIw", "tzIy xDu rThG"], ["cdIw tzIy", "xDu rThG"], ["cdIw tzIy xDu", "rThG"]]
+    testPartList 3, ["vJQ", "anj", "mQDq", "sOZ"],
+        [["vJQ", "anj mQDq sOZ"], ["vJQ anj", "mQDq sOZ"], ["vJQ anj mQDq", "sOZ"]]
+  end
+
+end
